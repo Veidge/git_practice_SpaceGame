5aef0782d540f243a785393986ab27168bf7b1ab
Veidge <alex.chesnokov.2007@gmail.com>
2025-05-18 13:10:37 +0300

    First job: creating game control

---
+++ game_functions.py
@@ -0,0 +1,50 @@
+import sys
+import pygame
+
+def check_events(ship):
+    """Обрабатывает нажатия клавиш и события мыши."""
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT:
+            sys.exit()
+        elif event.type == pygame.KEYDOWN:
+            if event.key == pygame.K_RIGHT:
+                ship.moving_right = True
+            elif event.key == pygame.K_LEFT:
+                ship.moving_left = True
+        elif event.type == pygame.KEYUP:
+                if event.key == pygame.K_RIGHT:
+                    ship.moving_right = False
+                elif event.key == pygame.K_LEFT:
+                    ship.moving_left = False
+                #Переместить корабль вправо.
+                ship.rect.centerx += 1
+
+def update_screen(ai_settings, screen, ship):
+    """Обновляет изображения на экране и отображает новый экран."""
+    screen.fill(ai_settings.bg_color)
+    ship.blitme()
+    pygame.display.flip()
+
+def check_keydown_events(event, ship):
+    """Реагирует на нажатие клавиш."""
+    if event.key == pygame.K_RIGHT:
+        ship.moving_right = True
+    elif event.key == pygame.K_LEFT:
+        ship.moving_left = True
+
+def check_keyup_events(event, ship):
+    """Реагирует на отпускание клавиш."""
+    if event.key == pygame.K_RIGHT:
+        ship.moving_right = False
+    elif event.key == pygame.K_LEFT:
+        ship.moving_left = False
+
+def check_events(ship):
+    """Обрабатывает нажатия клавиш и события мыши."""
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT:
+            sys.exit()
+        elif event.type == pygame.KEYDOWN:
+            check_keydown_events(event, ship)
+        elif event.type == pygame.KEYUP:
+            check_keyup_events(event, ship)
