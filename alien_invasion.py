8b608ea983e741971b8dbf8c82860d390a3351ac
Veidge <alex.chesnokov.2007@gmail.com>
2025-05-18 19:46:01 +0300

    Created a shooting and saved parts of the comments

--- alien_invasion.py
+++ alien_invasion.py
@@ -1,6 +1,7 @@
 import sys
 import pygame
 
+from pygame.sprite import Group 
 from settings import Settings
 from ship import Ship
 import game_functions as gf 
@@ -11,11 +12,16 @@ def run_game():
     screen = pygame.display.set_mode(
         (ai_settings.screen_width, ai_settings.screen_height))
     pygame.display.set_caption("Alien Invasion")
+     # Создание корабля.
     ship = Ship(ai_settings, screen)
+    # Создание группы для хранения пуль.
+    bullets = Group()
 
+     # Запуск основного цикла игры.
     while True:
-        gf.check_events(ship)
+        gf.check_events(ai_settings, screen, ship, bullets)
         ship.update()
-        gf.update_screen(ai_settings, screen, ship)
+        gf.update_bullets(bullets)
+        gf.update_screen(ai_settings, screen, ship, bullets)
 
 run_game()
--- ship.py
+++ ship.py
@@ -6,24 +6,29 @@ class Ship():
         self.screen = screen
         self.ai_settings = ai_settings
 
+        # Каждый новый корабль появляется у нижнего края экрана
         self.image = pygame.image.load('images/ship.bmp')
         self.rect = self.image.get_rect()
         self.screen_rect = screen.get_rect()
         self.rect.centerx = self.screen_rect.centerx
         self.rect.bottom = self.screen_rect.bottom 
-
+        
+        # Сохранение вещественной координаты центра корабля.
         self.center = float(self.rect.centerx)
 
+        # Флаги перемещения
         self.moving_right = False
         self.moving_left = False
 
     def update(self):
         """Обновляет позицию корабля с учётом флага."""
+        # Обновляется атрибут center, не rect.
         if self.moving_right and self.rect.right < self.screen_rect.right:
             self.center += self.ai_settings.ship_speed_factor
         if self.moving_left and self.rect.left > 0:
             self.center -= self.ai_settings.ship_speed_factor
-        
+
+         # Обновление атрибута rect на основании self.center
         self.rect.centerx = self.center
 
     def blitme(self):
